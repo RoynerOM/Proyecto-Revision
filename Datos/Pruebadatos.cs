29b4daa59ee08fb2b6dfc869238137ac99f3353d
AnnerDuarte <annerduarte1@gmail.com>
2021-05-30 19:18:47 -0600

    Prueba ca[a datos

---
+++ Datos/Pruebadatos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    class Pruebadatos
+    {
+        public void Pintar()
+        {
+            Console.WriteLine("Hola Mundo");
+        }
+    }
+}
