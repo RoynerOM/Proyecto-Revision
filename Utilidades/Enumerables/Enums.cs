951ad72ced71c998bab913c006296e0700b99c9a
RoynerOM <roportameza@gmail.com>
2021-07-11 14:34:54 -0600

    Ennumeradores

---
+++ Utilidades/Enumerables/Enums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilidades.Enumerables
+{
+    public class Enums
+    {
+        public enum MedioReceptivo
+        {
+            Correo,
+            Oficina,
+            Mensajero,
+            Whatsapp
+        }
+
+        public enum Estado
+        {
+            activo ,
+            inactivo
+        }
+
+    }
+}
